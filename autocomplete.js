07f6f6008d1b4bce3c2aef14c9f6f97080aa384f
Diego Vargas <diego_vargas@diego.2e-systems.com>
2018-08-23 16:31:49 +0200

    deleted unnecessary functionO

--- autocomplete.js
+++ autocomplete.js
@@ -10,13 +10,6 @@ class Autocomplete {
     this.event.simulated = true;
   }
 
-  dateToYMD(date) {
-    var d = date.getDate();
-    var m = date.getMonth() + 1; //Month from 0 to 11
-    var y = date.getFullYear();
-    return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
-  }
-
   fillInput() {
     for (var i = 0; i < this.frmInput.length; i++) {
 
