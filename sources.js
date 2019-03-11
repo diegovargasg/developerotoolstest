3e53cb7f30f5e373b7f36784a0a3fab7fd379ace
diegodevelopero <diegodevelopero@gmail.com>
2019-03-11 16:09:19 +0100

    Updated sources and fqtvNumber added

--- sources.js
+++ sources.js
@@ -2,19 +2,24 @@ const sources = {
   "firstName": ["Zacarias", "Kasimiro", "Rosa", "Benito", "Elber", "Hugo", "Melasuda", "Maria Dolores", "Elsa", "Debora", "Lola", "Lago", "Aquiles"],
   "lastName": ["Piedras", "Melchoto", "Melano", "Tucuca", "Camelas", "Galarga", "Norrea", "Rico", "Del Orto", "Porrico", "Teste", "Meraz", "Loza", "Baila"],
   "phoneNumber": ["12345567890", "0987654321"],
-  "email": ["diego@2e-systems.com", "diego.vargas@2e-systems.com", "vargas@2e-systems.com"],
+  "email": ["mail@2e-systems.com"],
+  "fqtvNumber": ["1000000000"],
   "cardHolder": ["Zacarias", "Kasimiro", "Rosa", "Benito", "Elber", "Hugo", "Melasuda", "Maria Dolores", "Elsa", "Debora", "Lola", "Lago", "Aquiles"],
-  "cardNumber": ["5555555555554444", "5454545454545454", "4917610000000000", "4911830000000", "4484070000000000"],
-  "expirationDate": ["12 / 18"],
-  "verificationCode": ["123"],
+  "cardNumber": ["4911830000000"],
+  "expirationDate": ["01 / 20"],
+  "verificationCode": [""], //usually 123
   "addressLine1": ["Fake Street 123", "742 Evergreen"],
   "city": ["Bad Soden", "Wiesbaden", "Frankfurt", "Zagreb"],
   "postalCode": ["65812"],
-  "emailAddr": ["diego@2e-systems.com", "diego.vargas@2e-systems.com", "vargas@2e-systems.com"],
-  "originCode": {"BMI": ["BRS"],
-                 "TCV": ["LIS"]},
-  "destinCode": {"BMI": ["ABZ", "FRA"],
-                 "TCV": ["SID"]},
+  "emailAddr": ["accept@accept.com"],
+  "originCode": {
+                  "TCV": ["BOS"],
+                  "BWA": ["FLL"]
+                },
+  "destinCode": {
+                  "TCV": ["RAI"],
+                  "BWA": ["POS"]
+                },
   "numAdt": [1, 2, 3],
   "numChd": [0, 1, 2],
   "departure": true,
